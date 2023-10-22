206d4012fae2a6458f491f5a3cdb0abb26026be8
Artur Lemes Moretti <artur.lmoretti@gmail.com>
2023-10-22 20:19:01 -0300

    Iniciando o projeto!!

---
+++ src/main/java/dio/santanderbootcampjavaangular/SantanderBootcampJavaAngularApplication.java
@@ -0,0 +1,13 @@
+package dio.santanderbootcampjavaangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderBootcampJavaAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderBootcampJavaAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/santanderbootcampjavaangular/SantanderBootcampJavaAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.santanderbootcampjavaangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBootcampJavaAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
